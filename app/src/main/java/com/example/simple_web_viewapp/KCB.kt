d667e3007d8ee88d9408de6b7527cf3b4b4e4bd2
Klein254 <rayobiero62@gmail.com>
2023-04-12 13:26:05 +0300

    KCB

--- app/src/main/java/com/example/simple_web_viewapp/KCB.kt
+++ app/src/main/java/com/example/simple_web_viewapp/KCB.kt
@@ -2,10 +2,19 @@ package com.example.simple_web_viewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class KCB : AppCompatActivity() {
+    lateinit var kcbWeb:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kcb)
+
+        kcbWeb = findViewById(R.id.kcbWeb)
+
+        val webSettings = kcbWeb.settings
+        webSettings.javaScriptEnabled = true
+        kcbWeb.loadUrl("https://ke.kcbgroup.com/")
+
     }
 }
