07fa77cc45a80cb87237d7be761400cf3c2c6931
Klein254 <rayobiero62@gmail.com>
2023-04-12 13:40:11 +0300

    absa

--- app/src/main/java/com/example/simple_web_viewapp/ABSA.kt
+++ app/src/main/java/com/example/simple_web_viewapp/ABSA.kt
@@ -2,10 +2,19 @@ package com.example.simple_web_viewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class ABSA : AppCompatActivity() {
+    lateinit var absaWeb: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_absa)
+
+        absaWeb = findViewById(R.id.absaWeb)
+
+        val webSettings = absaWeb.settings
+        webSettings.javaScriptEnabled = true
+        absaWeb.loadUrl("https://www.co-opbank.co.ke/")
     }
 }
